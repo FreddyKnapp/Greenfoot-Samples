8ca9cf1be585d22bfa042dbd1a926445e548a6df
MrYe070 <102677547+MrYe070@users.noreply.github.com>
2023-06-06 13:08:04 -0400

    Template Action subclasses for waiting and repeating

---
+++ Greenfoot-Samples/RepeatAction.java
@@ -0,0 +1,12 @@
+/**
+ * Action to repeat (i.e. loop) a series of actions a specified number of times.
+ */
+public class RepeatAction extends Action {
+    // TODO: Use a Script variable to store the series of actions to repeat.
+    
+    @Override
+    public boolean run(ScriptedActor actor) {
+        // TODO
+        return false;
+    }
+}
---
+++ Greenfoot-Samples/WaitAction.java
@@ -0,0 +1,10 @@
+/**
+ * Action to wait (do nothing) for a specified duration.
+ */
+public class WaitAction extends Action {
+    @Override
+    public boolean run(ScriptedActor actor) {
+        // TODO
+        return false;
+    }
+}
